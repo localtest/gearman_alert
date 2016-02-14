34f389be63431acecd7ed0ae6fff589ba5d52062
jiazhaoteng <jiazhaoteng@gmail.com>
2016-02-14 14:59:42 +0800

    add base config

---
+++ base.php
@@ -0,0 +1,6 @@
+<?php
+
+	define('BASE_NAME', dirname(__FILE__).'/');
+	include(BASE_NAME.'vendor/autoload.php');
+	include(BASE_NAME.'config/config_base.php');
+	include(BASE_NAME.'config/config_mailer.php');
