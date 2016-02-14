d2be37d4af2a295fd056fd83f484a743311cd972
jiazhaoteng <jiazhaoteng@gmail.com>
2016-02-14 14:17:02 +0800

    add mailer config

---
+++ config/config_mailer.php
@@ -0,0 +1,16 @@
+<?php
+
+	define('MAILER_HOST', 'smtp.126.com');
+	define('MAILER_PORT', 25);
+	define('MAILER_USER', 'from@test.com');
+	define('MAILER_USER_NAME', 'monitor');
+	define('MAILER_PASSWD', 'passwd');
+
+	$mail_group = array(
+		'all' => array(
+			array(
+				'name' => 'your name',
+				'email' => 'your email',
+			),
+		),
+	);
