3fa3d64cb90e0dc526822bbf625fc5f2133e6b7e
jiazhaoteng <jiazhaoteng@gmail.com>
2016-02-15 15:20:09 +0800

    add config item

--- config/config_base.php
+++ config/config_base.php
@@ -1,9 +1,11 @@
 <?php
 
 	define('LOG_PATH', BASE_NAME.'run/');
+	define('WEB_DOMAIN', 'http://your_domain/');
+
 	$monitors = array(
 		array(
-			'host' => '',
+			'host' => 'your_host',
 			'port' => 4730,
 			'interval' => 5,
 		),
