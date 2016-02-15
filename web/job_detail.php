0498254d65a42197425fb6630b8e511ff0882780
jiazhaoteng <jiazhaoteng@gmail.com>
2016-02-15 16:05:13 +0800

    add nav process

--- web/job_detail.php
+++ web/job_detail.php
@@ -6,8 +6,18 @@
 	$currDay = date('Y-m-d', time());
 	$date = isset($_GET['date']) && !empty($_GET['date']) ? $_GET['date'] : $currDay;
 
-	//10.10.68.117:4730_mailer_worker
+	function get_worker($job_str) {
+		$job = explode('_', $job_str);
+		array_shift($job);
+		$worker = implode('_', $job);
+		return $worker;
+	}
+
+	//host:port_worker
 	$job_str = isset($_GET['job_str']) && !empty($_GET['job_str']) ? $_GET['job_str'] : '';
 	$log_file = LOG_PATH.'monitor/'.$job_str.'_'.$date.'.log';
 
+	$worker = get_worker($job_str);
+	$title = $worker.' - 负载';
+	$show_return = true;
 	include(CURRENT_DIR.'template/detail.php');
