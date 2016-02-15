0978e79c1a8335ed0e65dd5bfaec640d7631d7c8
jiazhaoteng <jiazhaoteng@gmail.com>
2016-02-15 11:01:20 +0800

    add global timezone

--- base.php
+++ base.php
@@ -4,3 +4,5 @@
 	include(BASE_NAME.'vendor/autoload.php');
 	include(BASE_NAME.'config/config_base.php');
 	include(BASE_NAME.'config/config_mailer.php');
+
+	date_default_timezone_set("Asia/Chongqing");
